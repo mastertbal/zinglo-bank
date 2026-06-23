555a3cd0a9297e2bfa70b2cf5626ed87847b960f
mastertbal <mastertbal@gmail.com>
2026-06-23 15:26:02 +0100

    connected to database

---
+++ src/test/java/com/mastertbal/zinglobank/ZinglobankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastertbal.zinglobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZinglobankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
